b392c79235be58e8330553fb2b467626ddc3a2ed
Luke Pauley <notoriouslrp@gmail.com>
2017-12-13 10:51:02 -0500

    fix for single result

--- lib/pardot/query.rb
+++ lib/pardot/query.rb
@@ -6,6 +6,7 @@ module Pardot
       response = get(object, path, params)
       result = response["result"]
       result["total_results"] = result["total_results"].to_i if result["total_results"]
+      result[object] = [result[object]] if result[object].is_a?(Hash)
       result
     end
 
